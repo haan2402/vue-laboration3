56b935cbee55c47b8f3e3d4b140f39d756ae046b
Hanna Angeria <haan2402@student.miun.se>
2025-12-17 11:16:56 +0100

    gjort en grund för info sidan med html element och styling så jag kan fylla i text om reflektioner när jag genomfört uppgiften

--- src/views/infoView.vue
+++ src/views/infoView.vue
@@ -1,3 +1,48 @@
+<!--informationssidan-->
 <template>
-    <h2>Info sida</h2>
+    <section class="info-container">
+        <article>
+            <h2>Mina tankar om Vue</h2>
+            <p>
+                Fyll i text.................
+            </p>
+        </article>
+
+        <article>
+            <h2>Vad fungerade bra?</h2>
+            <p>
+                Fyll i text.................
+            </p>
+        </article>
+
+        <article>
+            <h2>Vad fungerade mindre bra?</h2>
+            <p>
+                Fyll i text.................
+            </p>
+        </article>
+    </section>
 </template>
+
+<!--styling för info sidan-->
+<style scoped>
+    .info-container {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+        justify-content: center;
+        align-items: center;
+    }
+
+    h2 {
+        font-size: 1.3em;
+        margin-bottom: 20px;
+    }
+
+    p {
+        font-size: 1em;
+        line-height: 1.5em;
+        margin-bottom: 20px;
+    }
+    
+</style>
