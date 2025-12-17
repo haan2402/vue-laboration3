69d5397845ac06d5e029b901320c4eb98887e875
Hanna Angeria <haan2402@student.miun.se>
2025-12-17 10:43:41 +0100

    skapat en footer komponent och lagt in text och styling för det

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+        <p>&copy; Bok listan</p>
+        <p>Frontend: Vue.js | Backend: Fastify & MongoDB</p>
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        background-color: #494949;
+        color: white;
+        padding: 20px;
+        text-align: center;
+        margin-top: 20px;
+    }
+</style>
