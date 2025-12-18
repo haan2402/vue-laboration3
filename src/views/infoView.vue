9870784d6aba5cdda58970fbee772f6b390cda81
Hanna Angeria <haan2402@student.miun.se>
2025-12-18 08:58:51 +0100

    fyllt i text på info sidan

--- src/views/infoView.vue
+++ src/views/infoView.vue
@@ -4,21 +4,25 @@
         <article>
             <h2>Mina tankar om Vue</h2>
             <p>
-                Fyll i text.................
+                Vue har varit ett bra ramverk att jobba med och det var enkelt att komma igång efter installation. Det gick att göra koden
+                strukturerad med olika komponenter och det gjorde att det var lätt att hålla koll på vad som händer i de olika delarna. 
             </p>
         </article>
 
         <article>
             <h2>Vad fungerade bra?</h2>
             <p>
-                Fyll i text.................
+                Det gick bra att jobba med olika komponenter och skicka data mellan dom med props och emits. Det var väldigt smidigt att skapa ett fungerande 
+                formulär med hjälp av Vues reaktiva data och v-model. Det var även smidigt att koppla ihop denna del av uppgiften med min REST-webbtjänst
+                från föregående moment.
             </p>
         </article>
 
         <article>
             <h2>Vad fungerade mindre bra?</h2>
             <p>
-                Fyll i text.................
+                När man stylar mer komponenter så var det lite svårt att få till CSS på allt i början. Jag fick justera några gånger för att få det att se bra
+                ut på både större skärmar och små skärmar, så det var lite svårt att komma in i från början. 
             </p>
         </article>
     </section>
@@ -42,8 +46,7 @@
     p {
         font-size: 1em;
         line-height: 1.5em;
-        margin-bottom: 30px;
-        max-width: 40em;
+        margin-bottom: 30px;   
     }
 
     @media (max-width: 600px) {
